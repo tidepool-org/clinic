ce261ac0c31ca7c71d9f2855905707b81f38af0a
Todd Kazakov <todd.kazakov@gmail.com>
2022-09-15 00:54:35 +0300

    Use master branch

--- artifact.sh
+++ artifact.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-wget -q -O artifact_go.sh 'https://raw.githubusercontent.com/tidepool-org/tools/sortable-tags/artifact/artifact.sh'
+wget -q -O artifact_go.sh 'https://raw.githubusercontent.com/tidepool-org/tools/master/artifact/artifact.sh'
 chmod +x artifact_go.sh
 
 . ./version.sh
