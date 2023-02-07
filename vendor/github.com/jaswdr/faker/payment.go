62608d4ae4a122c641e989f422c4d8e7b53732d4
lostlevels <lostlevels@users.noreply.github.com>
2023-02-07 10:51:19 -0800

    Bump mongodb driver package to v1.11 to support versions 5.0/6.0.

---
+++ vendor/github.com/jaswdr/faker/payment.go
@@ -0,0 +1,39 @@
+package faker
+
+import (
+	"strconv"
+)
+
+var (
+	cardVendors = []string{
+		"Visa", "Visa", "Visa", "Visa", "Visa",
+		"MasterCard", "MasterCard", "MasterCard", "MasterCard", "MasterCard",
+		"American Express", "Discover Card", "Visa Retired"}
+)
+
+// Payment is a faker struct for Payment
+type Payment struct {
+	Faker *Faker
+}
+
+// CreditCardType returns a fake credit card type for Payment
+func (p Payment) CreditCardType() string {
+	return p.Faker.RandomStringElement(cardVendors)
+}
+
+// CreditCardNumber returns a fake credit card number for Payment
+func (p Payment) CreditCardNumber() string {
+	return strconv.Itoa(p.Faker.IntBetween(1000000000000000, 9999999999999999))
+}
+
+// CreditCardExpirationDateString returns a fake credit card expiration date in string format for Payment
+func (p Payment) CreditCardExpirationDateString() string {
+	day := strconv.Itoa(p.Faker.IntBetween(0, 30))
+	if len(day) == 1 {
+		day = "0" + day
+	}
+
+	month := strconv.Itoa(p.Faker.IntBetween(12, 30))
+
+	return day + "/" + month
+}
