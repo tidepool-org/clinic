d9ad240295c20744d57e1c192fa5a8a2458d38b8
Todd Kazakov <todd.kazakov@gmail.com>
2023-11-16 11:51:48 +0200

    Unmarshal event metadata correctly

--- api/xealth.go
+++ api/xealth.go
@@ -1,36 +1,48 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/labstack/echo/v4"
 	"github.com/tidepool-org/clinic/errors"
 	"github.com/tidepool-org/clinic/xealth_models"
+	"io"
 	"net/http"
 )
 
 func (h *Handler) XealthPreorder(ec echo.Context) error {
 	ctx := ec.Request().Context()
 
-	// Make sure the request is initiated by redox
+	// Make sure the request is initiated by xealth
 	if err := h.xealth.AuthorizeRequest(ec.Request()); err != nil {
 		return err
 	}
 
-	type r struct {
-		xealth_models.PreorderFormRequest
-		EventContext string `json:"eventContext"`
-		EventType    string `json:"eventType"`
-	}
-	request := &r{}
-	if err := ec.Bind(&request); err != nil {
+	// Capture raw json for later processing
+	raw, err := io.ReadAll(ec.Request().Body)
+	if err != nil {
 		return err
 	}
 
-	if request.EventType != string(xealth_models.PreorderFormRequest0EventTypePreorder) {
-		return fmt.Errorf("%w: expected eventType='preorder' got %s", errors.BadRequest, request.EventType)
+	type meta struct {
+		EventType    string `json:"eventType"`
+		EventContext string `json:"eventContext"`
+	}
+	eventMeta := meta{}
+	if err := json.Unmarshal(raw, &eventMeta); err != nil {
+		return err
 	}
 
-	switch request.EventContext {
+	request := &xealth_models.PreorderFormRequest{}
+	if err := json.Unmarshal(raw, request); err != nil {
+		return err
+	}
+
+	if eventMeta.EventType != string(xealth_models.PreorderFormRequest0EventTypePreorder) {
+		return fmt.Errorf("%w: expected eventType='preorder' got %s", errors.BadRequest, eventMeta.EventType)
+	}
+
+	switch eventMeta.EventContext {
 	case string(xealth_models.PreorderFormRequest0EventContextInitial):
 		initial, err := request.AsPreorderFormRequest0()
 		if err != nil {
@@ -54,6 +66,6 @@ func (h *Handler) XealthPreorder(ec echo.Context) error {
 		}
 		return ec.JSON(http.StatusOK, response)
 	default:
-		return fmt.Errorf("%w: invalid event context %s", errors.BadRequest, request.EventContext)
+		return fmt.Errorf("%w: invalid event context %s", errors.BadRequest, eventMeta.EventContext)
 	}
 }
