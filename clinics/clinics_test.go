11a464d031f3a30dd01e5e57402f9365ee838047
Todd Kazakov <todd.kazakov@gmail.com>
2023-11-22 13:14:25 +0200

    Remove ginkgo v1

--- clinics/clinics_test.go
+++ clinics/clinics_test.go
@@ -1,7 +1,7 @@
 package clinics_test
 
 import (
-	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/tidepool-org/clinic/clinics"
 	"github.com/tidepool-org/clinic/clinics/test"
