230908e8259741967b0c4357e22e89a6586825e3
Alexander Diana <alex@rouk.org>
2024-06-12 20:42:05 +0000

    remove some now-unneeded code

--- xealth_client/fix_types.go
+++ xealth_client/fix_types.go
@@ -1,6 +1,6 @@
 package xealth_client
 
-// The following models were manually added, because oapi-codegen@v2.0.0 doesn't generate the required types
+// The following models were manually added, because oapi-codegen@v2.3.0 doesn't generate the required types
 // when an enum is defined inline. This code can be removed when https://github.com/deepmap/oapi-codegen/issues/1305
 // is resolved.
 
