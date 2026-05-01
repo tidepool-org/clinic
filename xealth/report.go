a80990919d5e09cc7dc811af06957ec13ec7a5fd
lostlevels <lostlevels@users.noreply.github.com>
2026-05-01 00:59:53 -0700

    Escape names w/ potential commas.

--- xealth/report.go
+++ xealth/report.go
@@ -1,11 +1,15 @@
 package xealth
 
 import (
+	"bytes"
+	"encoding/csv"
 	"fmt"
+	"net/url"
+	"strings"
+	"time"
+
 	"github.com/tidepool-org/clinic/clinics"
 	"github.com/tidepool-org/clinic/patients"
-	"net/url"
-	"time"
 )
 
 const (
@@ -69,7 +73,11 @@ func GenerateReportUrl(baseUrl string, token string, patient patients.Patient, c
 	case patients.GlycemicRangeTypeCustom:
 		query.Add("glycemicRangeType", string(patients.GlycemicRangeTypeCustom))
 		for _, threshold := range patient.GlycemicRanges.Custom.Thresholds {
-			query.Add("glycemicRangeThresholds", fmt.Sprintf("name,%s,upperBound.value,%f,upperBound.units,%s,inclusive,%v", threshold.Name, threshold.UpperBound.Value, threshold.UpperBound.Units, threshold.Inclusive))
+			name := &bytes.Buffer{}
+			w := csv.NewWriter(name)
+			w.Write([]string{threshold.Name})
+			w.Flush()
+			query.Add("glycemicRangeThresholds", fmt.Sprintf("name,%s,upperBound.value,%f,upperBound.units,%s,inclusive,%t", strings.TrimSpace(name.String()), threshold.UpperBound.Value, threshold.UpperBound.Units, threshold.Inclusive))
 		}
 	case patients.GlycemicRangeTypePreset:
 		query.Add("glycemicRangeType", string(patients.GlycemicRangeTypePreset))
