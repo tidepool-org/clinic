b67208e991f25d3f2f511320476dab62dc2fa917
Jimmy <lostlevels@users.noreply.github.com>
2026-07-30 09:12:55 -0700

    [BACK-4185][BACK-4519] Restrict patient export to clinic tiers 0200+. (#245)

--- api/patients.go
+++ api/patients.go
@@ -544,7 +544,6 @@ func (h *Handler) ExportPatientList(ec echo.Context, clinicId ClinicId, params E
 			Message: "expected user access token",
 		}
 	}
-
 	filter := export.Params{
 		Period:              params.Period,
 		ExporterClinicianID: authData.SubjectId,
