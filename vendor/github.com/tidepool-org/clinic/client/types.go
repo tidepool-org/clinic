941031459840227ad4b27f3b51152bb531f6cf79
Eric Wollesen <eric@tidepool.org>
2026-08-05 14:05:26 +0200

    regenerate to pick up latest target devices changes
    Before we start the migration, bring in the latest changes from the
    API.

--- vendor/github.com/tidepool-org/clinic/client/types.go
+++ vendor/github.com/tidepool-org/clinic/client/types.go
@@ -1418,6 +1418,12 @@ type EhrMatchResponseV1 struct {
 	Settings EhrSettingsV1 `json:"settings"`
 }
 
+// EhrNoteSettingsV1 defines model for ehrNoteSettings.v1.
+type EhrNoteSettingsV1 struct {
+	// IncludeGMI If true, include GMI in the notes.
+	IncludeGMI bool `json:"includeGMI,omitzero"`
+}
+
 // EhrProceduresV1 defines model for ehrProcedures.v1.
 type EhrProceduresV1 struct {
 	CreateAccount                 *string `json:"createAccount,omitempty"`
@@ -1436,6 +1442,7 @@ type EhrSettingsV1 struct {
 	Enabled        bool                   `json:"enabled"`
 	Flowsheets     EhrFlowsheetSettingsV1 `json:"flowsheets"`
 	MrnIdType      string                 `json:"mrnIdType"`
+	Notes          EhrNoteSettingsV1      `json:"notes,omitzero"`
 	ProcedureCodes EhrProceduresV1        `json:"procedureCodes"`
 	Provider       EhrSettingsV1Provider  `json:"provider"`
 
@@ -1603,10 +1610,18 @@ type PatientV1 struct {
 	Sites       []SiteV1              `json:"sites,omitzero"`
 
 	// Summary A summary of a patients recent data
-	Summary       *PatientSummaryV1 `json:"summary,omitempty"`
-	Tags          *PatientTagIdsV1  `json:"tags"`
-	TargetDevices *[]string         `json:"targetDevices,omitempty"`
-	UpdatedTime   *time.Time        `json:"updatedTime,omitempty"`
+	Summary *PatientSummaryV1 `json:"summary,omitempty"`
+	Tags    *PatientTagIdsV1  `json:"tags"`
+
+	// TargetDevices When uploading data, the user selects a target device (typically
+	// make and model) of their data source (i.e. meter or pump) from a
+	// list. This selection is used by the Tidepool Uploader to select the
+	// correct drivers for communicating with the data source.
+	//
+	// These values are stored in the user's profile so that they can be
+	// pre-selected on next use, thereby streamlining the process.
+	TargetDevices *TargetdevicesV1 `json:"targetDevices,omitempty"`
+	UpdatedTime   *time.Time       `json:"updatedTime,omitempty"`
 }
 
 // PatientClinicRelationshipV1 defines model for patientClinicRelationship.v1.
@@ -1856,6 +1871,15 @@ type SummaryIdV1 = string
 type SuppressedNotificationsV1 struct {
 	PatientClinicInvitation *bool `json:"patientClinicInvitation,omitempty"`
 }
+
+// TargetdevicesV1 When uploading data, the user selects a target device (typically
+// make and model) of their data source (i.e. meter or pump) from a
+// list. This selection is used by the Tidepool Uploader to select the
+// correct drivers for communicating with the data source.
+//
+// These values are stored in the user's profile so that they can be
+// pre-selected on next use, thereby streamlining the process.
+type TargetdevicesV1 = []string
 
 // TideConfigV1 defines model for tideConfig.v1.
 type TideConfigV1 struct {
