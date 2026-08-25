1345f741fd1c209ba01ebbcbe3a085e79d0367cc
lostlevels <lostlevels@users.noreply.github.com>
2026-08-24 22:26:06 -0700

    Updates from code review.

--- api/patients.go
+++ api/patients.go
@@ -551,6 +551,7 @@ func (h *Handler) ListBulkCreatePatients(ec echo.Context, clinicId ClinicId) err
 	res := ec.Response()
 	res.Header().Set(echo.HeaderContentType, "text/csv")
 	res.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=bulk-creation-result-%d.csv", time.Now().Unix()))
+	res.WriteHeader(http.StatusCreated)
 	return csv.NewWriter(res.Writer).WriteAll(outputRecords)
 }
 
@@ -577,5 +578,6 @@ func (h *Handler) BulkCreatePatients(ec echo.Context, clinicId ClinicId) error {
 	res := ec.Response()
 	res.Header().Set(echo.HeaderContentType, "text/csv")
 	res.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=bulk-creation-result-%d.csv", time.Now().Unix()))
+	res.WriteHeader(http.StatusCreated)
 	return csv.NewWriter(res.Writer).WriteAll(outputRecords)
 }
